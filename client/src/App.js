d1287647010da3c2c2a5266214de70252fd52a50
khgonzal <keheinke@gmail.com>
2022-03-01 15:13:02 -0500

    updates to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,52 @@
-import React from "react"
+import React, { useEffect } from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+
+import { getPosts } from './actions/posts'
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import unicorn from './images/unicorn.jpeg';
+import useStyles from './styles';
 
 const App = () => {
-    return (
-        <div>app</div>
-    )
-}
+  const classes = useStyles();
+  const dispatch = useDispatch();
 
-export default App
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+  return (
+    <Container maxWidth="xl">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Unicorn
+        </Typography>
+        <img
+          className={classes.image}
+          src={unicorn}
+          alt="unicorn"
+          height={60}
+        />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+};
+
+export default App;
